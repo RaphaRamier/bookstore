87a2f7c12a28fd26b91fe406cc49b1e1effda0a8
RaphaRamier <rapha.ramier@gmail.com>
2024-05-31 18:37:08 -0300

    feat: GenreCreateListView

--- genres/views.py
+++ genres/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from genres.models import Genre
+from genres.serializers import GenreSerializer
 
-# Create your views here.
+
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
