d375fe9c720f85c17612c7cea4ea3cf5c4d7fe7c
Raphael Ramier <rapha.ramier@gmail.com>
2024-06-05 18:56:52 -0300

    feat: Components Statistics: Monthly and Yearly

--- components/models.py
+++ components/models.py
@@ -9,6 +9,7 @@ class Component(models.Model):
     price_unit=models.DecimalField(max_digits=10, decimal_places=2)
     price_total=models.DecimalField(max_digits=10, decimal_places=2, editable=False)
     supplier=models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='components')
+    date = models.DateField()
 
     def __str__(self):
         return self.name
--- components/views.py
+++ components/views.py
@@ -1,4 +1,8 @@
+from django.db.models import Sum
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models.functions import TruncMonth, TruncYear
 from components.models import Component
 from components.serializers import ComponentSerializer
 
@@ -13,3 +17,45 @@ class ComponentRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     serializer_class=ComponentSerializer
 
 
+class SupplyMonthlyTrendView(APIView):
+    queryset=Component.objects.all()
+
+    def get(self, request):
+        monthly_trends=self.queryset.annotate(month=TruncMonth('date')).values('month').annotate(
+            total_quantity=Sum('quantity'),
+            total_value=Sum('price_total')
+        ).order_by('month')
+
+        data=[
+            {
+                'month': entry['month'].strftime('%Y-%m'),
+                'total_quantity': entry['total_quantity'],
+                'total_value': entry['total_value'],
+            }
+            for entry in monthly_trends
+        ]
+
+        return Response(data)
+
+
+class SupplyYearlyTrendView(APIView):
+    queryset=Component.objects.all()
+
+    def get(self, request):
+        yearly_trends=self.queryset.annotate(year=TruncYear('date')).values('year').annotate(
+            total_quantity=Sum('quantity'),
+            total_value=Sum('price_total')
+        ).order_by('year')
+
+        formatted_year=[
+            {
+                'date': entry['year'].strftime('%Y'),
+                'total_quantity': entry['total_quantity'],
+                'total_value': entry['total_value'],
+            }
+            for entry in yearly_trends
+        ]
+
+        data=formatted_year
+
+        return Response(data)
