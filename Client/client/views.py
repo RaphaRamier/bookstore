a3245a874e953712517e0cc01dc157ef8ca8422a
Raphael Ramier <rapha.ramier@gmail.com>
2024-06-15 12:59:32 -0300

    feat: Show last 20 transactions below chart

--- Client/client/views.py
+++ Client/client/views.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+from API.sales.models import Sale
 
 
 @login_required
 def home(request):
+    sales_list=Sale.objects.all()[:20]
+    paginator=Paginator(sales_list, 20)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
 
-
-
-
-    return render(request, 'cashflow/cashflow.html')
+    return render(request, 'cashflow/cashflow.html', {'page_obj':sales_list})
