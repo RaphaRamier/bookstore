6cfa6eb1086d961e3f7d163af1e5c2528c222f02
Raphael Ramier <rapha.ramier@gmail.com>
2024-06-20 22:16:46 -0300

    feat: Last Month sales diference

--- Client/client/views.py
+++ Client/client/views.py
@@ -6,18 +6,29 @@ from requests.auth import HTTPBasicAuth
 
 from API.cashflow.models import CashInFlow, CashOutFlow
 from API.sales.models import Sale
+from API.sales.views import SaleMonthlyTrendView
 from API.services.models import Service
 from Client import user
 
 
 @login_required
 def home(request):
+    sale_trend_view=SaleMonthlyTrendView()
+    response=sale_trend_view.get(request)
+
     sales=Sale.objects.all()
     sales_list=sales[:20]
     sales_done=sales[:10]
     total_inflow=round(CashInFlow.objects.filter(source__status='SUCCESSFUL').aggregate(total=Sum('amount'))['total'],
                        2) or 0
     total_outflow=round(CashOutFlow.objects.aggregate(total=Sum('amount'))['total'], 2)
+
+    percentage_difference = None
+    if 'monthly_trends' in response.data:
+        monthly_trends = response.data['monthly_trends']
+        if monthly_trends:
+            last_month_data = monthly_trends[0]
+            percentage_difference = round(last_month_data.get('percentage_difference'), 2)
 
     try:
         cash_flow=total_inflow - total_outflow
@@ -42,10 +53,13 @@ def home(request):
             'cash_flow': 0
         }
 
+    print(percentage_difference)
+
     return render(request, 'cashflow/cashflow.html',
                   {'sales_list': sales_list,
                    'sales_done': sales_done,
                    'top_sales': top_sales,
                    'cashflow': cashflow,
-                   'services_list': services_list
+                   'services_list': services_list,
+                   'percentage_difference': percentage_difference,
                    })
