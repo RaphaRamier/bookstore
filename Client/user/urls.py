62cd71b83d236820ca61d4bc1a89ee409df1be62
Raphael Ramier <rapha.ramier@gmail.com>
2024-06-30 19:20:37 -0300

    feat: Authors page (CRUD)

--- Client/user/urls.py
+++ Client/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from Client.user.views import loginpage
+from Client.user.views import loginpage, logout
 
 urlpatterns = [
-    path('login/', loginpage, name='login')
+    path('login/', loginpage, name='login'),
+    path('logout/', logout, name='logout')
 
 ]
--- Client/user/views.py
+++ Client/user/views.py
@@ -5,31 +5,31 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 
+from Client.user.forms import LoginForm
+
 
 def loginpage(request):
     if request.method == 'POST':
-        form=AuthenticationForm(request, data=request.POST)
+        form=LoginForm(request.POST)
         if form.is_valid():
-            username=form.cleaned_data.get('username')
-            password=form.cleaned_data.get('password')
-            user=authenticate(username=username, password=password)
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user=authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                messages.success(request, f'You are now logged in as {username}.')
-                return redirect('/home')
+                messages.success(request, f'{request.user.username} successfully logged in')
+                return redirect('home')  # Redirecione para a página inicial ou para onde você desejar
             else:
-                messages.error(request, 'Invalid username or password.')
-        else:
-            messages.error(request, 'Invalid username or password.')
+                form.add_error(None, 'Invalid username or password')
+                messages.error(request, 'Invalid username or password')
     else:
-        form=AuthenticationForm()
+        form=LoginForm()
 
     return render(request, 'user/login.html', {'form': form})
-
 
 
 @login_required
 def logout(request):
     auth.logout(request)
-    messages.success(request, 'Logout')
-    return redirect('loginpage')
+    messages.success(request, f'{request.user.username} Logged out')
+    return redirect('user/login.html')
